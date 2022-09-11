ecf4106e558bdc1f04a7dd8a3f95f4beb598f605
cmarciao <marciaocassio@gmail.com>
2022-09-11 14:29:35 -0300

    Add react-toastify lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { ToastContainer } from "react-toastify";
 import { TodoList } from "./pages/TodoList";
+
 import "./styles/global.css";
+import "react-toastify/ReactToastify.min.css";
 
 export function App() {
   return (
     <>
+      <ToastContainer
+        theme="dark"
+        position="top-right"
+        autoClose={1000 * 3}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        pauseOnHover={false}
+      />
       <TodoList />
     </>
   );
